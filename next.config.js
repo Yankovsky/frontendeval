3be5fab53c6bbe9e67f203100fab05be51649bbc
yankovskyandrey <YankovskyAndrey@gmail.com>
2022-05-02 16:44:24 +0300

    best ever

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.BASE_PATH || '',
-  assetPrefix: process.env.BASE_PATH || '',
+  basePath: process.env.BASE_PATH || ''
 }
 
 module.exports = nextConfig
